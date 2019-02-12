f3d44870cf3876d238420cdc3db93a1d19072690
VieuxLoupGris <provocemois@gmail.com>
2019-02-12 18:15:02 +0100

    Fonctiannal cat cifer

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ import 'bootstrap';
 
 console.log("Hey look in your browser console. It works!");
 
-$('.carousel').carousel({
-  interval: 10
-});
+import images from "./images/*.jpg";
+
+let i = 1;
+let countImg = Object.keys(images).length
+
+console.log(countImg);
+
+setInterval( ()=>{
+  document.getElementById("CatImg").src =images["cat"+i];
+  console.log(images["cat"+i]);
+  i++;
+  if (i == 12){
+    i = 1
+  }
+}, 1000)
